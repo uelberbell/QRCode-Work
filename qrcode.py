fdbb08a9ce916bda4e4bd05fca807624de045222
uelberbell <72271381+uelberbell@users.noreply.github.com>
2020-10-08 09:39:14 -0300

    .txt finish
    Consegui jogar os códigos para o arquivo .txt
    agora preciso ler esses dados e gerar o QRCODE

--- qrcode.py
+++ qrcode.py
@@ -6,18 +6,9 @@ import sys
 
 
 cdBarras = []
-#print('DIGITE "0" A QUALQUER MOMENTO PARA ENCERRAR O PROGRAMA!')
 
-#while(cdBarras != 0):
- #   cdBarras = str (input('Digite o código de barras: '))
-  #  #s += "\nEnter" comando para gerar a tecla enter após captar o código de barras.
-   # if(cdBarras == '0'):
-    #    print('SISTEMA ENCERRADO')
-     #   break
 print('            ||||||||CONVERTER CÓDIGO DE BARRAS PARA QRCODE||||||||')
 print('            _____________________________________________________\n')
-
-
 
 #Aqui nós pediremos ao usuário a quantidade de produtos que ele irá inserir 
 n = int (input('Digite a quantidade de produtos que serão lidos: '))
@@ -26,27 +17,25 @@ n = int (input('Digite a quantidade de produtos que serão lidos: '))
 for i in range(0, n):
     print("Digite o código de barras" , i + 1, ":")
     item = input()  
-    cdBarras.append(item)
-    print(item)
+    arquivo = open('codigos.txt' , 'a')
+    arquivo.write(item + "\nEnter\n")
+    print("Nome do arquivo de texto: " , arquivo.name)
+    arquivo.close()
+    #cdBarras.append(item)
+    #print(cdBarras)
+    
+arquivo = open('codigos.txt' , 'r')
+for linha in arquivo:
+    linha = linha.rstrip()
+    print(linha)
+arquivo.close()   
 
-#Exibindo lista de códigos capturados    
-print("\n Dados do QRCODE: ", cdBarras)
 
-#Pegando dados capturados e jogando numa unica variável para criar o Qrcode
-lista = str (cdBarras)
-#Convertendo para String
-
-#Tentativa de formatação de lista
- 
-lista = str(cdBarras).strip("[],''")
-
-
-url = pyqrcode.create(lista)
+url = pyqrcode.create('codigos.txt')
 n = input('\nDigite o nome do cliente: ')    
 url.svg(n +".svg" , scale = 8)
 url.png(n + ".png" , scale = 6)
 print('\n>>>>QRCODE SALVO NA PASTA DO PROGRAMA<<<<')
-print("\n Dados do QRCODE: ", dados)
 
 
 
