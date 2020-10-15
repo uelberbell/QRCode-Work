1b6d1d2491729ff947c9c4a0b7b91e48a2280587
uelberbell <72271381+uelberbell@users.noreply.github.com>
2020-10-15 09:20:38 -0300

    Projeto pausado
    Ainda é preciso ajustar a captura dos dados contidos em cada linha do arquivo .txt gerado, pois preciso que gere o qrcode com a mesma formatação contida no arquivo.
    A função pyqrcode.create() precisa de um parametro de captura do arquivo.

--- qrcode.py
+++ qrcode.py
@@ -1,38 +1,42 @@
 import pyqrcode
+import qrcode
 from pyqrcode import QRCode
 import array as arr
 import png
 import sys
 
 
-cdBarras = []
-
 print('            ||||||||CONVERTER CÓDIGO DE BARRAS PARA QRCODE||||||||')
 print('            _____________________________________________________\n')
 
-#Aqui nós pediremos ao usuário a quantidade de produtos que ele irá inserir 
+
+
+#Aqui nós pediremos ao usuário a quantidade de produtos que ele irá vender 
 n = int (input('Digite a quantidade de produtos que serão lidos: '))
 
+      
+      
 #Condição para para solicitar quantas vezes o programa vai solicitar dados
 for i in range(0, n):
     print("Digite o código de barras" , i + 1, ":")
-    item = input()  
+    item = input()
     arquivo = open('codigos.txt' , 'a')
     arquivo.write(item + "\nEnter\n")
     print("Nome do arquivo de texto: " , arquivo.name)
     arquivo.close()
-    #cdBarras.append(item)
-    #print(cdBarras)
-    
+        
+
+
 arquivo = open('codigos.txt' , 'r')
-for linha in arquivo:
+for linha in arquivo:           
     linha = linha.rstrip()
     print(linha)
 arquivo.close()   
 
 
-url = pyqrcode.create('codigos.txt')
-n = input('\nDigite o nome do cliente: ')    
+#Ainda preciso achar uma forma de ler cada linha do arquivo .txt e criar o qrcode com a formatação contida dentro do arquivo.
+url = pyqrcode.create("ainda precisamos saber o que entrar aqui para ler todo arquivo txt com os códigos de barras")
+n = input('\nDigite o nome do cliente: ')  
 url.svg(n +".svg" , scale = 8)
 url.png(n + ".png" , scale = 6)
 print('\n>>>>QRCODE SALVO NA PASTA DO PROGRAMA<<<<')
